04fc3c4bd58c2b480813570200351f063030ad55
kujawa-konrad <106272820+kujawa-konrad@users.noreply.github.com>
2022-05-26 07:23:24 +0200

    Update budget.py

--- budget.py
+++ budget.py
@@ -130,18 +130,3 @@ def create_spend_chart(ledger_list):
     chart = chart.rstrip('\n')
     return chart
 
-
-food = Category("Food")
-food.deposit(1000, "initial deposit")
-food.withdraw(10.15, "groceries")
-food.withdraw(15.89, "restaurant and more food for dessert")
-# print(food.get_balance())
-clothing = Category("Clothing")
-food.transfer(50, clothing)
-clothing.withdraw(25.55)
-clothing.withdraw(100)
-
-print(food)
-print(clothing)
-
-print(create_spend_chart([food, clothing]))
